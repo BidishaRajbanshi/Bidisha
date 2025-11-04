beb99d4ab3e8b17a8b2fed3f4110716ad69e230a
BidishaRajbanshi <bidisha.ad24@bmsce.ac.in>
2025-11-04 15:59:33 +0545

    Add files via upload

---
+++ Tower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void tower(int n, char from, char to, char aux) {
+    if (n == 1) {
+        printf("Move disk 1 from rod %c to rod %c\n", from, to);
+        return;
+    }
+    tower(n - 1, from, aux, to);
+    printf("Move disk %d from rod %c to rod %c\n", n, from, to);
+    tower(n - 1, aux, to, from);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of disks: ");
+    scanf("%d", &n);
+    tower(n, 'A', 'C', 'B');
+    return 0;
+}
